01c3622e846be38baacbd86e9a495fb1b1e66692
sakib124024 <sakib124024@yahoo.com>
2024-04-22 16:43:43 +0100

    cahnges made

--- src/main/java/com/oocode/HomePage.java
+++ src/main/java/com/oocode/HomePage.java
@@ -5,7 +5,8 @@ public class HomePage {
             <html>
                 <body>
                 <h1>Answers questions</h1>
-                <p>This is a minimal web app for a lab</p>
+                <p>This is a minimal web app for a lab
+                a change has been made</p>
                 </body>
             </html>""".trim();
 }
